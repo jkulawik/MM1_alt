3afa33bd6a51b3456a9fc28acf2292c916d33387
Jacek <stvoreku@gmail.com>
2021-05-21 14:58:59 +0200

    bugg

--- brudno.py
+++ brudno.py
@@ -1,9 +1,10 @@
 from functions import exp
 import random
 from models import Event, Packet,CrashOn
+import matplotlib.pyplot as plt
 
 
-random.seed(5)
+random.seed(2)
 # sredni czas obslugi pakietu przez serwer
 time_of_service = 0.125
 # sredni odstep pomiedzy pakietami 0.5 - 4
@@ -47,6 +48,7 @@ event_list.append(Event(time=exp(c_on), obj=crash, type='crash_on'))
 # sortowanie listy zdarzen po czasie
 event_list.sort(key=lambda Event: Event.time)
 crash_list = []
+packet_delay_list = []
 
 
 packetsCounter += 1
@@ -58,15 +60,16 @@ while number_of_serviced_packets < number_of_packets_for_simulation:
     event_list.sort(key=lambda Event: Event.time)
     event = event_list.pop(0)
     clock = event.time
+    print(f'TIME: {clock} EVENT: {event.type}')
     if event.type == 'end_packet':
         number_of_serviced_packets += 1
         curr_p = event.obj
         curr_p.finish_of_service = clock
-        print(f'PROCESSED ARRIVE: {curr_p.time_of_arrival} AT: {curr_p.finish_of_service}')
+        #print(f'PROCESSED ARRIVE: {curr_p.time_of_arrival} AT: {curr_p.finish_of_service}')
         print('END PACKET')
         if len(packets_buffor) == 0:
             is_server_busy = False
-        else:
+        elif not is_server_dead:
             is_server_busy = True
             curr_p = packets_buffor.pop(0)
             event_list.append(Event(time=clock+curr_p.time_of_service, type='end_packet', obj=curr_p))
@@ -75,7 +78,9 @@ while number_of_serviced_packets < number_of_packets_for_simulation:
         #obecny pakiet trafia na bufor
         packets_buffor.append(event.obj)
         #nastepny pakiet
-        next_p = Packet(time_of_arrival=clock+exp(time_next_packet), time_of_service=exp(time_of_service))
+        t_delay = exp(average_time_between_packets)
+        packet_delay_list.append(t_delay)
+        next_p = Packet(time_of_arrival=clock+t_delay, time_of_service=exp(time_of_service))
         event_list.append(Event(time=next_p.time_of_arrival, obj=next_p, type='new_packet'))
         packets.append(next_p)
         #jesli wszystko ok to przetwarzamy najstarszy pakiet
@@ -93,20 +98,30 @@ while number_of_serviced_packets < number_of_packets_for_simulation:
             if a.type == 'end_packet':
                 a.time += curr_crash.duration
     elif event.type == 'crash_off':
-        print('CRASH OFF')
+        print(f'CRASH OFF {clock}')
         is_server_dead = False
         crash_duration = exp(c_off)
         curr_crash = CrashOn(crash_duration)
         event_list.append(Event(time=exp(c_on)+clock, obj=curr_crash, type='crash_on'))
 
 sum_time = 0
+sum_processed = 0
+delays_list = []
 for p in packets:
     if p.finish_of_service != 0:
         sum_time += (p.finish_of_service - p.time_of_arrival)
-        print(f'{p.finish_of_service} {p.time_of_arrival}')
-
-average = sum_time/len(packets)
-print(average)
+        delays_list.append((p.finish_of_service - p.time_of_arrival))
+        sum_processed += 1
+        #print(f'{p.finish_of_service} {p.time_of_arrival}')
+plt.plot(delays_list, 'r+', markersize=2)
+plt.show()
+plt.plot(crash_list, 'bo', markersize=2)
+plt.show()
+plt.plot(packet_delay_list, 'go', markersize=2)
+plt.show()
+print('AVG packet delay: {}'.format(sum(packet_delay_list)/len(packet_delay_list)))
+average = sum_time/sum_processed
+print('PRACTICAL DELAY: {}'.format(average))
 propability_off = c_off / (c_off + c_on)
 propability_on = c_on / (c_off + c_on)
 
@@ -118,6 +133,6 @@ p = LAMBDA / (u*propability_on)
 if p != 1:
     # Sredni czas oczekiwania w systemie - kalkulacje teoretyczne
     teoretical_average_delay = (p + LAMBDA*c_off*propability_off) / (LAMBDA * (1 - p))
-    print(teoretical_average_delay)
+    print("TEORETICAL DELAY: {}".format(teoretical_average_delay))
 else:
     pass
